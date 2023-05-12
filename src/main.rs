cebf89cd4372f3cbab22d49e817d73619989b716
Jethro Ordaneza <jet.oneza@gmail.com>
2023-05-12 22:55:21 +0800

    Install tokio for running async

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Cycling Trainer made using Rust!");
+
+    connect().await;
+}
+
+async fn connect() {
+    println!("Connecting to trainer...");
 }
