e128f9f601fa5b00697e2ef12f933d7a02e652a0
Jethro Ordaneza <jet.oneza@gmail.com>
2023-05-14 15:24:02 +0800

    Implement the generic finding of device in main

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-use btleplug::api::{Central, ScanFilter};
 use std::error::Error;
-use std::time::Duration;
 
-mod  ble;
+mod ble;
 
 use ble::bluetooth::Bluetooth;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let bluetooth = Bluetooth::new().await;
-    let central = bluetooth.get_central().await.unwrap();
 
-    central.start_scan(ScanFilter::default()).await?;
-
-    tokio::time::sleep(Duration::from_secs(5)).await;
-
-    let trainer_name = "KICKR CORE AF30";
-    let trainer = bluetooth.find_trainer("KICKR CORE AF30").await;
-
-    println!("Peripheral for {}: {:?}", trainer_name, trainer);
+    find_device(&bluetooth, "KICKR CORE AF30").await;
+    find_device(&bluetooth, "MX Master 3").await;
 
     bluetooth.list_devices().await?;
 
     Ok(())
+}
+
+async fn find_device(bluetooth: &Bluetooth, name: &str) {
+    let device = bluetooth.find_device(name).await;
+
+    println!("Peripheral for {}: {:?}", name, device);
 }
