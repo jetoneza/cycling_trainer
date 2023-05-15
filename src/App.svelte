b1965c5ec9e7b6563498e740f20cd89dd70be4c3
Jethro Ordaneza <jethro@poplarhomes.com>
2023-05-15 21:55:52 +0800

    Install tauri

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<style>
+.logo.rust:hover {
+  filter: drop-shadow(0 0 2em #ff3e00);
+}
+</style>
+
+<script lang="ts">
+import Greet from './lib/Greet.svelte'
+</script>
+
+<main class="container">
+  <h1>Cycling Trainer!</h1>
+
+  <div class="row">
+    <a href="https://www.rust-lang.org/" target="_blank">
+      <img src="/rust.svg" class="logo rust" alt="Rust Logo" />
+    </a>
+  </div>
+
+  <div class="row">
+    <Greet />
+  </div>
+</main>
