6aab561977ac3813de76e95032263799971ab357
Jethro Ordaneza <jethro@poplarhomes.com>
2023-05-16 21:09:05 +0800

    Create find_device command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+mod ble;
+
+use ble::bluetooth::Bluetooth;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn find_device(query: &str) -> Result<String, ()> {
+    let bluetooth = Bluetooth::new().await;
+
+    let device = bluetooth.find_device(query).await;
+
+    Ok(format!("Peripheral for {}: {:?}", query, device))
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![find_device])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
