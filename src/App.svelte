69bd66d8ac3a3a0282b1fef52715c4a86de81952
Jethro Ordaneza <jet.oneza@gmail.com>
2023-05-31 16:39:20 +0800

    Add initial UI layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,22 @@
-<style>
-.logo.rust {
-  filter: drop-shadow(0 0 2em #ff3e00);
-}
-</style>
-
 <script lang="ts">
-import Bluetooth from './lib/Bluetooth.svelte'
+import Splash from './components/Splash.svelte'
+import Nav from './components/Nav.svelte'
+
+// States
+let appStarted = false
+
+// Functions
+function initApp() {
+  // TODO: Add initializations here
+
+  appStarted = true
+}
 </script>
 
 <main class="container">
-  <h1>Cycling Trainer!</h1>
-
-  <div class="row">
-    <a href="https://www.rust-lang.org/" target="_blank">
-      <img src="/rust.svg" class="logo rust" alt="Rust Logo" />
-    </a>
-  </div>
-
-  <div class="row">
-    <Bluetooth />
-  </div>
+  {#if !appStarted}
+    <Splash on:initApp="{initApp}" />
+  {:else}
+    <Nav />
+  {/if}
 </main>
