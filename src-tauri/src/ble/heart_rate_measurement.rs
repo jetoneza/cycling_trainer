8155a5b2c44ce14cfd720aba4e40b6285fd76088
Jethro Ordaneza <jet.oneza@gmail.com>
2023-06-10 21:22:38 +0800

    Add todo in heart rate measurement struct

--- src-tauri/src/ble/heart_rate_measurement.rs
+++ src-tauri/src/ble/heart_rate_measurement.rs
@@ -4,6 +4,8 @@ pub struct HeartRateMeasurement {
 }
 
 pub fn parse_hrm_data(data: &Vec<u8>) -> HeartRateMeasurement {
+    // TODO: Complete parsing of data
+    // e.g. detection if hrm monitor is in contact of body
     HeartRateMeasurement {
         bpm: data[1] as u16,
     }
