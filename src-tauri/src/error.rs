65cb100c365936674bdb0f82335a40a6fbea8fd7
Jethro Ordaneza <jet.oneza@gmail.com>
2023-06-17 14:03:49 +0800

    Create error and prelude modules

---
+++ src-tauri/src/error.rs
@@ -0,0 +1,21 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Generic {0}")]
+    Generic(String),
+
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+}
+
+impl serde::Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
+}
+
+pub fn error_generic(message: &str) -> Error {
+    Error::Generic(message.into())
+}
