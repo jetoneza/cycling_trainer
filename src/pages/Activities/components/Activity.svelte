dc8eb7277087596a3b46e71583e62f7ca2ed0c50
Jethro Ordaneza <jet.oneza@gmail.com>
2023-07-12 11:29:50 +0800

    feat(activities): add initial UI for activity viewer

--- src/pages/Activities/components/Activity.svelte
+++ src/pages/Activities/components/Activity.svelte
@@ -2,11 +2,20 @@
 import type { Activity } from 'src/types'
 
 export let selectedActivity: Activity
+export let handleStartActivity: () => void
 </script>
 
 {#if !!selectedActivity}
-  <div class="activity-viewer flex-1 p-6">
-    <div class="title text-2xl font-bold">{selectedActivity.name}</div>
-    <div class="decription">{selectedActivity.description}</div>
+  <div class="activity-viewer flex-1 p-6 flex flex-col space-y-4">
+    <div class="title-wrapper">
+      <div class="title text-2xl font-bold">{selectedActivity.name}</div>
+      <div class="decription">{selectedActivity.description}</div>
+    </div>
+
+    <div class="actions">
+      <button class="btn text-md" on:click="{handleStartActivity}"
+        >Start Workout</button
+      >
+    </div>
   </div>
 {/if}
