28bd9f1b864c56859f610949c6b06e8b068a1e1e
Jethro Ordaneza <jet.oneza@gmail.com>
2023-07-14 01:16:08 +0800

    refactor: declare svelte-icons in typescript declaration file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare module 'svelte-icons/fa/FaBolt.svelte'
+declare module 'svelte-icons/fa/FaHeartbeat.svelte'
+declare module 'svelte-icons/fa/FaRegClock.svelte'
+declare module 'svelte-icons/go/GoSync.svelte'
+declare module 'svelte-icons/io/IoMdSpeedometer.svelte'
+declare module 'svelte-icons/md/MdDirectionsBike.svelte'
