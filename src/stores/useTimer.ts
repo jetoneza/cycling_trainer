f2cd0ead046f0154613dc01f212f77da456ac709
Jethro Ordaneza <jet.oneza@gmail.com>
2023-07-14 14:16:20 +0800

    feat(workout): create initial implementation of the timer

---
+++ src/stores/useTimer.ts
@@ -0,0 +1,46 @@
+import { writable, get } from 'svelte/store'
+
+export enum TimerStatus {
+  Started = 'started',
+  Paused = 'paused',
+  Stopped = 'stopped',
+}
+
+export const useTimer = () => {
+  const elapsedTime = writable(0)
+  const intervalTime = writable(0)
+  const status = writable(TimerStatus.Stopped)
+
+  let timer: NodeJS.Timer
+
+  const start = () => {
+    status.set(TimerStatus.Started)
+
+    timer = setInterval(() => {
+      elapsedTime.update((time) => time + 1)
+      intervalTime.update((time) => time + 1)
+    }, 1000)
+  }
+
+  // TODO: Implement pause
+
+  const stop = () => {
+    clearInterval(timer)
+
+    status.set(TimerStatus.Stopped)
+    elapsedTime.set(0)
+    intervalTime.set(0)
+  }
+
+  const getStatus = () => get(status)
+  const resetInterval = () => intervalTime.set(0)
+
+  return {
+    elapsedTime,
+    intervalTime,
+    getStatus,
+    start,
+    stop,
+    resetInterval,
+  }
+}
