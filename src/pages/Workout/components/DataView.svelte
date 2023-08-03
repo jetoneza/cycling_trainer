e900530d69726d577eab97a32d2dc453a2a2463e
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-03 20:30:07 +0800

    feat(workout): add direction arrows if value is over/under the target value for power/cadence

--- src/pages/Workout/components/DataView.svelte
+++ src/pages/Workout/components/DataView.svelte
@@ -3,6 +3,8 @@
 import HeartIcon from 'svelte-icons/fa/FaHeartbeat.svelte'
 import LightningIcon from 'svelte-icons/fa/FaBolt.svelte'
 import CadenceIcon from 'svelte-icons/go/GoSync.svelte'
+import UpArrowIcon from 'svelte-icons/fa/FaLongArrowAltUp.svelte'
+import DownArrowIcon from 'svelte-icons/fa/FaLongArrowAltDown.svelte'
 
 // Types
 import { DataType, type BasicObject } from '../../../types'
@@ -10,16 +12,66 @@ import { DataType, type BasicObject } from '../../../types'
 const CADENCE_TARGET_MARGIN = 5
 const POWER_TARGET_MARGIN = 10
 
+enum TargetRange {
+  Under = 'under',
+  Over = 'over',
+  Within = 'within',
+}
+
 // Props
 export let devices: BasicObject
 
 $: hasTargetCadence =
   !!devices[DataType.TargetPower] && devices[DataType.TargetCadence].value > 0
 
-const getColorClass = (target: number, value: number, margin: number): string =>
-  value >= target - margin && value <= target + margin
-    ? 'text-white'
-    : 'text-red-500'
+const getColorClass = (
+  target: number,
+  value: number,
+  margin: number
+): string => {
+  const targetRange = getTargetRange(target, value, margin)
+
+  return targetRange === TargetRange.Within ? 'text-white' : 'text-red-500'
+}
+
+const getTargetRange = (
+  target: number,
+  value: number,
+  margin: number
+): TargetRange => {
+  if (value >= target - margin && value <= target + margin) {
+    return TargetRange.Within
+  }
+
+  if (value < target + margin) {
+    return TargetRange.Under
+  }
+
+  return TargetRange.Over
+}
+
+$: powerColorClass = getColorClass(
+  devices[DataType.TargetPower].value,
+  devices[DataType.Power].value,
+  POWER_TARGET_MARGIN
+)
+
+$: powerTargetRange = getTargetRange(
+  devices[DataType.TargetPower].value,
+  devices[DataType.Power].value,
+  POWER_TARGET_MARGIN
+)
+
+$: cadenceTargetRange = getTargetRange(
+  devices[DataType.TargetCadence].value,
+  devices[DataType.Cadence].value,
+  CADENCE_TARGET_MARGIN
+)
+$: cadenceColorClass = getColorClass(
+  devices[DataType.TargetCadence].value,
+  devices[DataType.Cadence].value,
+  CADENCE_TARGET_MARGIN
+)
 </script>
 
 <div class="data-view flex justify-between rounded-lg bg-secondary-200">
@@ -56,12 +108,18 @@ const getColorClass = (target: number, value: number, margin: number): string =>
   <div class="column px-6 pb-4 pt-2">
     <div class="row item text-center">
       <div
-        class="value flex w-full justify-center text-9xl font-bold {getColorClass(
-          devices[DataType.TargetCadence].value,
-          devices[DataType.Cadence].value,
-          POWER_TARGET_MARGIN
-        )}"
+        class="value flex w-full justify-center text-9xl font-bold {powerColorClass}"
       >
+        {#if powerTargetRange !== TargetRange.Within}
+          <div class="icon ml-2 mt-11 w-6 animate-bounce">
+            {#if powerTargetRange === TargetRange.Under}
+              <UpArrowIcon />
+            {:else}
+              <DownArrowIcon />
+            {/if}
+          </div>
+        {/if}
+
         {devices[DataType.Power].value}
         <div class="icon ml-2 mt-11 w-10">
           <LightningIcon />
@@ -80,13 +138,17 @@ const getColorClass = (target: number, value: number, margin: number): string =>
       </div>
 
       <div class="item">
-        <div
-          class="value flex text-4xl font-bold {getColorClass(
-            devices[DataType.TargetCadence].value,
-            devices[DataType.Cadence].value,
-            CADENCE_TARGET_MARGIN
-          )}"
-        >
+        <div class="value flex text-4xl font-bold {cadenceColorClass}">
+          {#if cadenceTargetRange !== TargetRange.Within}
+            <div class="icon ml-1 mt-3 w-3 animate-bounce">
+              {#if cadenceTargetRange === TargetRange.Under}
+                <UpArrowIcon />
+              {:else}
+                <DownArrowIcon />
+              {/if}
+            </div>
+          {/if}
+
           {devices[DataType.Cadence].value}
           <div class="icon ml-1 mt-3 h-5 w-5">
             <CadenceIcon />
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 
 declare module 'svelte-icons/fa/FaBolt.svelte'
 declare module 'svelte-icons/fa/FaHeartbeat.svelte'
+declare module 'svelte-icons/fa/FaLongArrowAltUp.svelte'
+declare module 'svelte-icons/fa/FaLongArrowAltDown.svelte'
 declare module 'svelte-icons/fa/FaRegClock.svelte'
 declare module 'svelte-icons/go/GoSync.svelte'
 declare module 'svelte-icons/io/IoMdSpeedometer.svelte'
