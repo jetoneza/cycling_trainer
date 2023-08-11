62bd18b70e4814b729bee14e5656fbf44933cd8e
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-11 12:49:02 +0800

    feat(workout): create initial UI for summary modal

---
+++ src/pages/Workout/components/Summary.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+export let onBackClick: () => void
+export let onSaveClick: () => void
+</script>
+
+<div class="summary">
+  <div class="text-2xl text-center font-bold">Session Summary</div>
+  <!-- TODO: Add summary graphs -->
+
+  <div class="actions absolute bottom-12 flex w-full justify-center space-x-6">
+    <button class="btn secondary w-1/6 text-2xl" on:click="{onBackClick}">
+      Back
+    </button>
+    <button class="btn w-1/6 text-2xl" on:click="{onSaveClick}"> Save </button>
+  </div>
+</div>
