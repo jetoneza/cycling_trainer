d0e05753ef0dad0aca8d1ab89134db34e32e6fe8
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-11 12:05:57 +0800

    feat(workout): add pause menu

---
+++ src/pages/Workout/components/Menu.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import PlayIcon from 'svelte-icons/fa/FaPlay.svelte'
+import StopIcon from 'svelte-icons/fa/FaStop.svelte'
+
+// export const onSessionStop = () => {}
+export let onSessionStop: () => void
+</script>
+
+<div class="menu flex justify-center overflow-hidden">
+  <button class="border-r-2 border-gray-500 px-6">
+    <div class="icon w-10">
+      <PlayIcon />
+    </div>
+  </button>
+  <button class="px-6" on:click="{onSessionStop}">
+    <div class="icon w-10">
+      <StopIcon />
+    </div>
+  </button>
+</div>
